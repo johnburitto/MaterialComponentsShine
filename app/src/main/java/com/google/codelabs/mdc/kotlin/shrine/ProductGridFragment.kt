9596e91eebdc96221f54b16a9e5781bd18dcb647
Андрій Борсук <johnburitto@gmail.com>
2023-04-14 17:17:23 +0300

    Add frid of Card by using RecycleView

--- app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
+++ app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductGridFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.codelabs.mdc.kotlin.shrine.network.ProductEntry
 import kotlinx.android.synthetic.main.shr_product_grid_fragment.view.*
 
 class ProductGridFragment : Fragment() {
@@ -20,6 +23,18 @@ class ProductGridFragment : Fragment() {
         val view = inflater.inflate(R.layout.shr_product_grid_fragment, container, false)
 
         (activity as AppCompatActivity).setSupportActionBar(view.app_bar)
+
+        view.recycler_view.setHasFixedSize(true)
+        view.recycler_view.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+
+        val adapter = ProductCardRecyclerViewAdapter(ProductEntry.initProductEntryList(resources))
+
+        view.recycler_view.adapter = adapter
+
+        val largePadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing)
+        val smallPadding = resources.getDimensionPixelSize(R.dimen.shr_product_grid_spacing_small)
+
+        view.recycler_view.addItemDecoration(ProductGridItemDecoration(largePadding, smallPadding))
 
         return view
     }
